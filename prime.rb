a80591bc8dd77a175e8af24e2a64d3e1148a20e7
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-04-24 19:37:58 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,6 @@ def prime?(int)
   if int <= 1
     return false
   else
-    (2..sqrt(int)).to_a.all {|factor| int % factor != 0}
+    (2..int**0.5).to_a.all? {|factor| int % factor != 0}
   end
 end
