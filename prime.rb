8dff8c9d49b2341ff486126afb929550a8014d50
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-04-24 19:46:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,8 @@
 # Add  code here!
-require 'benchmark'
-
 def prime?(int)
   if int <= 1
     return false
   else
     (2..int**0.5).to_a.all? {|factor| int % factor != 0}
   end
-puts Benchmark.measure { "prime?()"*1_000_000 }
 end
